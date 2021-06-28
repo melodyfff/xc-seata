3ec6355e1f3a222a9bbe100ffc60fc74893a6d84
melodyfff <307208327@qq.com>
2021-06-28 23:42:07 +0800

    :smile: init

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# xc-seata
-nacos+seata
+# Getting Started
+
+### Reference Documentation
+
+For further reference, please consider the following sections:
+* [spring-cloud-alibaba版本说明](https://github.com/alibaba/spring-cloud-alibaba/wiki/%E7%89%88%E6%9C%AC%E8%AF%B4%E6%98%8E)
+
+### Guides
+
+The following guides illustrate how to use some features concretely:
+* [nacos quick start](https://nacos.io/zh-cn/docs/quick-start.html)
+* [gitee: nacos-server](https://gitee.com/mirrors/Nacos/tags)
+* [SEATA官网文档](http://seata.io/zh-cn/docs/overview/what-is-seata.html)
+
---
+++ nacos-config-center/src/main/java/com/xinchen/nacos/ConfigController.java
@@ -0,0 +1,25 @@
+package com.xinchen.nacos;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * curl http://localhost:8080/config/get
+ */
+@RestController
+@RequestMapping("/config")
+@RefreshScope
+public class ConfigController {
+    @Value("${useLocalCache:true}")
+    private boolean useLocalCache;
+
+    /**
+     * http://localhost:8080/config/get
+     */
+    @RequestMapping("/get")
+    public boolean get() {
+        return useLocalCache;
+    }
+}
