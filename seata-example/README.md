e6ea12e6e1751c750a12de96c75e276b866d3a28
melodyfff <307208327@qq.com>
2021-06-29 17:24:19 +0800

    :zap: [seata-example-order]新增示例

---
+++ seata-example/README.md
@@ -0,0 +1,5 @@
+# 简单seata demo
+
+## Reference Documentation
+
+[看了 5种分布式事务方案，我司最终选择了 Seata，真香！](https://zhuanlan.zhihu.com/p/315164700)
---
+++ seata-example/order/src/main/java/com/xinchen/controller/OrderController.java
@@ -0,0 +1,45 @@
+package com.xinchen.controller;
+
+import com.xinchen.entity.Order;
+import com.xinchen.service.OrderService;
+import java.math.BigDecimal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ */
+@RestController
+@RequestMapping(value = "order")
+class OrderController {
+  private final OrderService orderService;
+
+  OrderController(OrderService orderService) {
+    this.orderService = orderService;
+  }
+
+  /**
+   * 创建订单
+   * @param order
+   * @return
+   */
+  @GetMapping("create")
+  public String create(Order order){
+    orderService.create(order);
+    return "Create order success";
+  }
+
+  /**
+   * 修改订单状态
+   * @param userId
+   * @param money
+   * @param status
+   * @return
+   */
+  @RequestMapping("update")
+  String update(@RequestParam("userId") Long userId, @RequestParam("money") BigDecimal money, @RequestParam("status") Integer status){
+    orderService.update(userId,money,status);
+    return "订单状态修改成功";
+  }
+}
